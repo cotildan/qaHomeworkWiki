fce74923d7498d93951c83c9d451915bb04db32c
Cotilda <cotilda@Cotildas-MacBook-Air.local>
2022-05-14 14:16:17 -0700

    added comments

--- 2.1/basicUnitTestStarter.test.ts
+++ 2.1/basicUnitTestStarter.test.ts
@@ -1,24 +1,69 @@
 describe("Testing the calculator", () => {
     test("Example test", () => {
+      /* 
+      checking that using the add() that 
+       2 + 50 = 52 
+       */
       expect(calculator.add(2, 50)).toBe(52);
     });
+
     test("Addition works", () => {
+      /* 
+
+      checking that using the add() in the calculator that 
+       4 + 5 = 9 
+       */
       expect(calculator.add(4,5)).toBe(9);
     });
+
     test("Subtraction works", () => {
+      /* 
+
+      checking that using the subtract() in the calculator that 
+       5 - 4 = 1 
+       */
       expect(calculator.subtract(5,4)).toBe(1);
     });
+
     test("Multiplication works", () => {
+      /* 
+
+      checking that using the multiply() in the calculator that 
+       4 * 5 = 20 
+       */
       expect(calculator.multiply(4,5)).toBe(20);
     });
+
     test("Division works", () => {
+      /* 
+      
+      checking that using the divide() in the calculator that 
+       20 / 4 = 5 
+       */
       expect(calculator.divide(20,4)).toBe(5);
     });
+
     describe("Stretch Goals", () => {
+      /*
+
+      checking that using the all operations in the calculator that 
+      (2+2)/4 = 1 
+      (4)/4 = 1
+      */
       test("(2+2)/4 == 1", () => {
         expect(calculator.divide(calculator.add(2, 2), 4)).toBe(1);
       });
+
       test("(28/4)*(6/2)*(30-27+(3-4)) == 42", () => {
+      /*
+      
+      checking that using the all operations in the calculator that 
+      (28/4)*(6/2)*(30-27+(3-4)) = 42
+      (7)*(3)*(30-27+(-1)) = 42
+      (7)*(3)*(3+(-1)) = 42
+      (7)*(3)*(2) = 42
+      (21)*(2) = 42
+      */
         expect(calculator.multiply(calculator.multiply(calculator.divide(28,4),calculator.divide(6,2)),
         calculator.add(calculator.subtract(30,27),calculator.subtract(3,4)))).toBe(42);
       });
--- 2.2/newFunction.test.ts
+++ 2.2/newFunction.test.ts
@@ -2,15 +2,19 @@ import { myFunction } from "./newFunction";
 
 describe("myFunction", () => {
   it("returns the word 'true' if I pass the function the number 5", () => {
+    // checking when myfunction EQUALS 5 it should say true
     expect(myFunction(5)).toBe("true");
   });
   it("returns the word 'false' if I pass in a number less than 5", () => {
+    // checking when myfunction < 5  it should say  false
     expect(myFunction(1)).toBe("false");
   });
   it("returns the word 'big' if I pass in a numbewr greater than 5", () => {
+    // checking when myfunction > 5  it should say  big
     expect(myFunction(100)).toBe("big");
   });
   it("returns the word 'negative' if a negative number is passed in", () => {
+    // checking when myfunction > 0 it should say  negative
     expect(myFunction(-1)).toBe("negative");
   });
 });
